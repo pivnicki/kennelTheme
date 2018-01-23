c8069739e813e5fcc18e9070b049b1aeb1a86794
Stevan Pivnicki <stevan314@gmail.com>
2018-01-23 13:08:40 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Gyongy Szigeti</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+  <link rel="stylesheet" href="assets/css/style.css"> 
+</head>
+<body>
+
+<div class="container">
+<header>
+	<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#services">SERVICES</a></li>
+        <li><a href="#portfolio">PORTFOLIO</a></li>
+        <li><a href="#pricing">PRICING</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
+
+	<div class="row">
+		<div class="col-md-12">
+		<!-- Carousel-->
+			<div class="carousel slide" id="carousel-633954">
+				<ol class="carousel-indicators">
+					<li class="active" data-slide-to="0" data-target="#carousel-633954">
+					</li>
+					<li data-slide-to="1" data-target="#carousel-633954">
+					</li>
+					<li data-slide-to="2" data-target="#carousel-633954">
+					</li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="item active">
+						<img alt="Carousel Bootstrap First" src="http://lorempixel.com/output/sports-q-c-1600-500-1.jpg" />
+						<div class="carousel-caption">
+							<h4>
+								First Thumbnail label
+							</h4>
+							<p>
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap Second" src="http://lorempixel.com/output/sports-q-c-1600-500-2.jpg" />
+						<div class="carousel-caption">
+							<h4>
+								Second Thumbnail label
+							</h4>
+							<p>
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap Third" src="http://lorempixel.com/output/sports-q-c-1600-500-3.jpg" />
+						<div class="carousel-caption">
+							<h4>
+								Third Thumbnail label
+							</h4>
+							<p>
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+						</div>
+					</div>
+				</div> <a class="left carousel-control" href="#carousel-633954" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-633954" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div><!-- ./Carousel -->
+			
+			<!-- News -->
+			<div class="row">
+				<div class="col-md-8">
+					<div class="row">
+					<h2 class="line"><span>News</span></h2>
+						<div class="col-md-4">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+								<div class="caption">
+									<h3>
+										Thumbnail label
+									</h3>
+									<p>
+										Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+									</p>
+									<p>
+										<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+								<div class="caption">
+									<h3>
+										Thumbnail label
+									</h3>
+									<p>
+										Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+									</p>
+									<p>
+										<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+								<div class="caption">
+									<h3>
+										Thumbnail label
+									</h3>
+									<p>
+										Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+									</p>
+									<p>
+										<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+								<div class="caption">
+									<h3>
+										Thumbnail label
+									</h3>
+									<p>
+										Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+									</p>
+									<p>
+										<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+								<div class="caption">
+									<h3>
+										Thumbnail label
+									</h3>
+									<p>
+										Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+									</p>
+									<p>
+										<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+								<div class="caption">
+									<h3>
+										Thumbnail label
+									</h3>
+									<p>
+										Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+									</p>
+									<p>
+										<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="list-group">
+						 <a href="#" class="list-group-item active">Home</a>
+						<div class="list-group-item">
+							List header
+						</div>
+						<div class="list-group-item">
+							<h4 class="list-group-item-heading">
+								List group item heading
+							</h4>
+							<p class="list-group-item-text">
+								...
+							</p>
+						</div>
+						<div class="list-group-item">
+							<span class="badge">14</span>Help
+						</div> <a class="list-group-item active"><span class="badge">14</span>Help</a>
+					</div>
+					<div class="list-group">
+						 <a href="#" class="list-group-item active">Home</a>
+						<div class="list-group-item">
+							List header
+						</div>
+						<div class="list-group-item">
+							<h4 class="list-group-item-heading">
+								List group item heading
+							</h4>
+							<p class="list-group-item-text">
+								...
+							</p>
+						</div>
+						<div class="list-group-item">
+							<span class="badge">14</span>Help
+						</div> <a class="list-group-item active"><span class="badge">14</span>Help</a>
+					</div>
+				</div>
+			</div>
+			<!-- ./News -->
+			<div class="row">
+				<div class="col-md-6">
+					<form class="form-horizontal" role="form">
+						<div class="form-group">
+							 
+							<label for="inputEmail3" class="col-sm-2 control-label">
+								Email
+							</label>
+							<div class="col-sm-10">
+								<input type="email" class="form-control" id="inputEmail3" />
+							</div>
+						</div>
+						<div class="form-group">
+							 
+							<label for="inputPassword3" class="col-sm-2 control-label">
+								Password
+							</label>
+							<div class="col-sm-10">
+								<input type="password" class="form-control" id="inputPassword3" />
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<div class="checkbox">
+									 
+									<label>
+										<input type="checkbox" /> Remember me
+									</label>
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								 
+								<button type="submit" class="btn btn-default">
+									Sign in
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="col-md-6">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								Panel title
+							</h3>
+						</div>
+						<div class="panel-body">
+							Panel content
+						</div>
+						<div class="panel-footer">
+							Panel footer
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<footer>
+All rights Reserved
+</footer>
+</body>
